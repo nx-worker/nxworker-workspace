aec5e69554feb2e5f9211b4a88d65c83018d6c2e
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2025-10-03 00:34:11 +0200

    feat: generate `github-actions-post-status-check` application
    Command used:
    ```shell
    nx g @nx/node:application --name=github-actions-post-status-check --directory=.github/actions/post-status-check --e2eTestRunner=none --js=true --projectNameAndRootFormat=as-provided --swcJest=true
    ```

---
+++ .github/actions/post-status-check/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  displayName: 'github-actions-post-status-check',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: { syntax: 'typescript', tsx: true },
+          transform: { react: { runtime: 'automatic' } },
+        },
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/.github/actions/post-status-check',
+};
